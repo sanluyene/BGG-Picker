c12f137b9d5c7780abb2c2575fceffcb6666e148
Ashley Pressley <ashley.marie.pressley@gmail.com>
2019-11-06 17:58:58 -0500

    game detail inclusion

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var app = angular.module("bggDecision", []);
-app.controller("bggCtrl", function($scope, $http, $cookies) {
+app.controller("bggCtrl", function($scope, $http) {
   $scope.collectionIds = [];
-  $scope.collection = {};
-  $scope.username = $cookies.get('username');
+  $scope.collection = [];
+  //$scope.username = $cookies.get('username');
 
   $scope.callBGG = function() {
     if ($scope.username == null) alert('Please enter a username.');
     else {
-      $cookies.put('username', $scope.username);
+      //$cookies.put('username', $scope.username);
       $scope.urlIds = 'https://www.boardgamegeek.com/xmlapi2/collection?username=' + $scope.username;
 
       $http({
@@ -19,35 +19,20 @@ app.controller("bggCtrl", function($scope, $http, $cookies) {
         $scope.jqX = $($scope.xml);
         $scope.jqX.find('item').each(function () {
           //console.log($(this).attr('objectid'));
-          $scope.collectionIds.push($(this).attr('objectid'));
-          $scope.urlItem = 'https://www.boardgamegeek.com/xmlapi2/thing?id=' + $(this).attr('objectid');
-
-          $http({
-            method  : 'GET',
-            url     : $scope.urlItem,
-            timeout : 10000
-          }).then(function success(response) {
-            $scope.xmlI = $.parseXML(response.data);
-            $scope.jqXI = $($scope.xmlI);
-            // make object with attrs
-            // min player
-            // max player
-            // complexity
-            // categories
-            // name
-            // link
-            // min play time
-            // max play time
-            // your rating
-
-          },function failure(response) {
-            // alert("Something went wrong.");
-          });
+          $scope.game = new Game($(this).attr('objectid'));
+          $scope.collection.push($scope.game);
         })
       },function failure(response) {
         alert("Something went wrong.");
       });
     }
+  }
+
+  $scope.filterGames = function(game) {
+    var players = document.getElementById("numPlayers").value;
+    if (players >= parseInt(game.minPlayers) && players <= parseInt(game.maxPlayers)) return true;
+
+    return false;
   }
 });
 
@@ -76,3 +61,46 @@ $("#time-range").slider({
 });
 
 $("#time-amount").html($("#time-range").slider("values", 0) + " - " + $("#time-range").slider("values", 1));
+
+function Game(objectid) {
+  var minPlayers = '';
+  var maxPlayers = '';
+  var minTime = '';
+  var maxTime = '';
+  var complexity = '';
+  var name = '';
+  var thumb = '';
+
+  this.id = objectid;
+  this.url = 'https://www.boardgamegeek.com/xmlapi2/thing?id=' + objectid + '&stats=1';
+
+  $.ajax({
+    type: "GET",
+    url: this.url,
+    async: false,
+    dataType: 'xml',
+    success: function success(response) {
+      // console.log(response);
+      // console.log($(response).find("items").children('item').find('statistics').find('ratings').find('averageweight').attr('value'));
+
+      minPlayers = $(response).find("items").children('item').find('minplayers').attr('value');
+      maxPlayers = $(response).find("items").children('item').find('maxplayers').attr('value');
+      minTime = $(response).find("items").children('item').find('minplaytime').attr('value');
+      maxTime = $(response).find("items").children('item').find('maxplaytime').attr('value');
+      complexity = $(response).find("items").children('item').find('statistics').find('ratings').find('averageweight').attr('value');
+      name = $(response).find("items").children('item').find('name').attr('value');
+      thumb = $(response).find("items").children('item').find('thumbnail').text();
+
+      // categories
+      // your rating
+    }
+  });
+
+  this.minPlayers = minPlayers;
+  this.maxPlayers = maxPlayers;
+  this.minTime = minTime;
+  this.maxTime = maxTime;
+  this.complexity = complexity;
+  this.name = name;
+  this.thumb = thumb;
+}
