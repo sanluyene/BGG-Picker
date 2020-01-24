82b5a662c2eda50b7a065f3a3dda94c7171125d8
Ashley Pressley <ashley.marie.pressley@gmail.com>
2020-01-24 16:13:03 -0500

    beautify, vscode workspace

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var app = angular.module("bggDecision", []);
-app.controller("bggCtrl", function($scope, $http) {
+app.controller("bggCtrl", function ($scope, $http) {
   $scope.collectionIds = [];
   $scope.collection = [];
   //$scope.username = $cookies.get('username');
 
-  $scope.callBGG = function() {
+  $scope.callBGG = function () {
     if ($scope.username == null) alert('Please enter a username.');
     else {
       //$cookies.put('username', $scope.username);
       $scope.urlIds = 'https://www.boardgamegeek.com/xmlapi2/collection?username=' + $scope.username;
 
       $http({
-        method  : 'GET',
-        url     : $scope.urlIds,
-        timeout : 10000
+        method: 'GET',
+        url: $scope.urlIds,
+        timeout: 10000
       }).then(function success(response) {
         $scope.xml = $.parseXML(response.data);
         $scope.jqX = $($scope.xml);
@@ -22,13 +22,13 @@ app.controller("bggCtrl", function($scope, $http) {
           $scope.game = new Game($(this).attr('objectid'));
           $scope.collection.push($scope.game);
         })
-      },function failure(response) {
+      }, function failure(response) {
         alert("Something went wrong.");
       });
     }
   }
 
-  $scope.filterGames = function(game) {
+  $scope.filterGames = function (game) {
     var players = document.getElementById("numPlayers").value;
     if (players >= parseInt(game.minPlayers) && players <= parseInt(game.maxPlayers)) return true;
 
@@ -42,8 +42,8 @@ $("#complexity-range").slider({
   max: 5,
   step: 0.01,
   values: [0, 5],
-  slide: function(event, ui) {
-    $("#complexity-amount").html(ui.values[ 0 ] + " - " + ui.values[ 1 ]);
+  slide: function (event, ui) {
+    $("#complexity-amount").html(ui.values[0] + " - " + ui.values[1]);
   }
 });
 
@@ -55,8 +55,8 @@ $("#time-range").slider({
   max: 300,
   step: 5,
   values: [0, 300],
-  slide: function(event, ui) {
-    $("#time-amount").html(ui.values[ 0 ] + " - " + ui.values[ 1 ]);
+  slide: function (event, ui) {
+    $("#time-amount").html(ui.values[0] + " - " + ui.values[1]);
   }
 });
 
