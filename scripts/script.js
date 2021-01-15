a427e088b0605009c516b8ce55cb93cfb5931dc8
sanluyene <ashley.marie.pressley@gmail.com>
2021-01-15 11:15:16 -0500

    initial code copy-over

--- scripts/script.js
+++ scripts/script.js
@@ -185,6 +185,109 @@ bgApp.controller("bggCtrl", function ($scope, $http) {
   }
 });
 
+bgApp.controller("swfCtrl", function ($scope, $http) {
+  // Default values
+  $scope.numPlayers = '4';
+  $scope.username = 'sanluyene'; // Testing
+
+  $scope.categories = [
+    'Pirates',
+    'Zombies'
+  ];
+
+  $scope.collectionIds = [];
+  $scope.collection = [];
+  //$scope.username = $cookies.get('username');
+
+  $scope.callSteam = async function () {
+    if ($scope.username == null) alert('Please enter a username.');
+    else {
+      // const collection = await $scope.GetCollection();
+    }
+  }
+
+  $scope.GetCollection = function () {
+    //$cookies.put('username', $scope.username);
+    let urlIds = 'https://www.boardgamegeek.com/xmlapi2/collection?own=1&username=' + $scope.username;
+    let header = {};
+
+    $http({
+      method: 'GET',
+      url: urlIds,
+      headers: header,
+      timeout: 10000
+    }).then(function (response) {
+      if (response.status == 202) {
+        setTimeout(function () {
+          callswf();
+        }, 3000)
+      } else if (response.status == 200) {
+        // Success
+        const xml = $.parseXML(response.data);
+        const jqX = $(xml);
+        jqX.find('item').each(function () {
+          $scope.collectionIds.push($(this).attr('objectid'));
+        });
+
+        return $scope.GetGames();
+      }
+    }, function (response) {
+      // Failure
+      console.log("Something went wrong.");
+      console.log(response);
+    });
+  }
+
+  $scope.GetGames = function () {
+    // After we've collected the Game IDs, we can get each of their details
+    const ids = Object.values($scope.collectionIds);
+    const id_string = ids.join();
+    const urlGs = 'https://www.boardgamegeek.com/xmlapi2/thing?id=' + id_string + '&stats=1';
+    let header = {};
+
+    $http({
+      method: 'GET',
+      url: urlGs,
+      headers: header,
+      timeout: 10000
+    }).then(function (response) {
+      if (response.status == 202) {
+        setTimeout(function () {
+          GetGames();
+        }, 3000)
+      } else if (response.status == 200) {
+        // Success
+        let xml = $.parseXML(response.data);
+        let jqX = $(xml);
+        jqX.find('item').each(function () {
+          let id = $(this).attr('objectid');
+          let url = 'https://www.boardgamegeek.com/xmlapi2/thing?id=' + id + '&stats=1';
+          let name = $(this).find('name').attr('value');
+          let minPlayers = $(this).find('minplayers').attr('value');
+          let maxPlayers = $(this).find('maxplayers').attr('value');
+          let minTime = $(this).find('minplaytime').attr('value');
+          let maxTime = $(this).find('maxplaytime').attr('value');
+          let complexity = $(this).find('statistics').find('ratings').find('averageweight').attr('value');
+          let thumb = $(this).find('thumbnail').text();
+
+          // Denotes full game or expansion
+          let gameType = $(this).attr('type');
+
+          // TODO get personal rating
+          // TODO get categories
+
+          let game = new Game(id, url, name, minPlayers, maxPlayers, minTime, maxTime, complexity, thumb, gameType);
+          $scope.collection.push(game);
+        })
+      }
+    }, function (response) {
+      // Failure
+      console.log("Something went wrong.");
+      console.log(response);
+    });
+  }
+});
+
 bgApp.filter('filterGames', function () {
   return function (items, numPlayers, complexityMax, complexityMin, time, expansions) {
     var newItems = [];
@@ -297,4 +400,18 @@ class Game {
     this.thumb = thumb;
     this.gameType = gameType;
   }
+}
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
